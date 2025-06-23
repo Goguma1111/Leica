fd97a586690bc20b1f9b938a36f2f318c60f77fd
goguma <doffltm112543@gmail.com>
2025-06-23 11:27:18 +0900

    시작

---
+++ src/main/java/kr/goguma/leica/LeicaApplication.java
@@ -0,0 +1,13 @@
+package kr.goguma.leica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/goguma/leica/LeicaApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.goguma.leica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
