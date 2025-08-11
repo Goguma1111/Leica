f286cf02907d0be7e1a335fc12e4f9e7fbc59b09
sia <esia15600@gmail.com>
2025-08-11 14:28:45 +0900

    feat: 백엔드 구현

--- backend/main/java/kr/goguma/leica/soppha/mapper/MemberMapper.java
+++ backend/main/java/kr/goguma/leica/soppha/mapper/MemberMapper.java
@@ -1,23 +1,41 @@
+package kr.goguma.leica.soppha.mapper;
 
+import java.util.List;
 import java.util.Map;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import kr.goguma.leica.soppha.models.NewMember;
 
 @Mapper
 public interface MemberMapper {
 
-//가입 인원수 집계
-@Insert ("insert into NewMember (date, count) "
-        + "VALUES (#{date}, #{count})")
-        public void insertMember(Map<String, Object> params);
+        //가입 인원수 집계
+        @Insert ("insert into NewMember (date, `count`) "
+                + "SELECT DATE(regdate) AS date, COUNT(*) AS count "
+                + "FROM Member "
+                + "WHERE DATE(regdate) = DATE(DATE_ADD(NOW(), INTERVAL -1 DAY))"
+                + "GROUP BY DATE(regdate)")
+                public void insertMember(NewMember Member);
 
 
+        //주간별 가입 인원수 조회
         @Select("SELECT "
         + "date, count(*)"
         + "FROM NewMember "
-        + "WHERE date = #{date}")
-        public int countNewMembers(@Param("date") String date);
+        + "WHERE date >= DATE(DATE_ADD(NOW(), INTERVAL -7 DAY)) "
+        + "GROUP BY date")
+        public List<NewMember> getDailyNewMembers();
 
+        //일별 가입자 통계
+        @Select("SELECT "
+        + "DATE_FORMAT(regdate, '%Y-%M' ) AS WEEKEND, "
+        + "DATE_FORMAT(DATE_SUB(`regdate`, INTERVAL (DAYOFWEEK(regdate)-1) DAY), '%Y/%m/%d') AS Week_Start, "
+        + "DATE_FORMAT(DATE_SUB(`regdate`, INTERVAL (DAYOFWEEK(regdate)-7) DAY), '%Y/%m/%d') AS Week_End, "
+        + "SUM(count) AS count "
+        + "FROM NewMember "
+        + "WHERE DATE(regdate) >= DATE(DATE_ADD(NOW(), INTERVAL -7 DAY)) "
+        + "GROUP BY WEEKEND, Week_Start, Week_End ")
+        List<NewMember> getWeeklyNewMembers();
 }
