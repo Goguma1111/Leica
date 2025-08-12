17c31b920638b140a5d41c832123e8d5435fcafc
sia <esia15600@gmail.com>
2025-08-12 13:06:53 +0900

    fix: resolve merge conflicts in LeicaApplication.java and MemberMapper.java

--- backend/main/java/kr/goguma/leica/LeicaApplication.java
+++ backend/main/java/kr/goguma/leica/LeicaApplication.java
@@ -10,7 +10,8 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@MapperScan({"kr.goguma.leica.goguma1111.Mapper", "kr.goguma.leica.Seo.mapper", "kr.goguma.leica.mapper", "kr.goguma.leica.soppha.mapper"})
+
+@MapperScan({"kr.goguma.leica.mapper", "kr.goguma.leica.Seo.mapper","kr.goguma.leica.soppha.mapper"})
 @ComponentScan(basePackages = "kr.goguma.leica")
 @EnableScheduling
 public class LeicaApplication {
